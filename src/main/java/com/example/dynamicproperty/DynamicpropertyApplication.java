246b2791ce345374779165a1f130b4a0b4721568
chhayas <chhayas@hpe.com>
2018-08-30 15:17:30 +0530

    adding spring boot file

---
+++ src/main/java/com/example/dynamicproperty/DynamicpropertyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicpropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicpropertyApplication.class, args);
+        System.out.println("Jaspreet Singh");
+    }
+}
