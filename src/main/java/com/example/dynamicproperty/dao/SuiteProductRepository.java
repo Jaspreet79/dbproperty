e2bb596c52442bcc653e0ecfa0d451f655da763e
Jaspreet79 <jaspreet79@gmail.com>
2018-09-02 14:59:20 +0530

    Create SuiteProductRepository.java

---
+++ src/main/java/com/example/dynamicproperty/dao/SuiteProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.dynamicproperty.dao;
+
+import com.example.dynamicproperty.model.SuiteProduct;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SuiteProductRepository extends CrudRepository<SuiteProduct,String> {
+}
