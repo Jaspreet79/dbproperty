79f574c87c08e48883cd1fb5eb7e25ba0b44b3ed
Jaspreet79 <jaspreet79@gmail.com>
2018-09-04 16:28:14 +0530

    Seed Script for suite product table

--- src/main/resources/db/db-1/oracle/db_suite_product.sql
+++ src/main/resources/db/db-1/oracle/db_suite_product.sql
@@ -1,1 +1,16 @@
 
+DECLARE
+	l_count INTEGER;
+BEGIN
+
+---------------------------Supported Properties for different products------------
+
+select count(*) into l_count FROM db_suite_product where suite_product_id = 1 AND product_name='CSA';
+IF (l_count = 0 ) THEN
+    Insert into db_suite_product
+       (suite_product_id,product_name, display_name,description,created_on,updated_on)
+    Values
+       (1,'CSA','CLOUD SERVICE AUTOMATION','Hybrid Cloud Management Solution',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+END IF;
+
+END;
