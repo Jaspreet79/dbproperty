38ebb2550098d207d42ed0e6289d5586dbdf85c7
Jaspreet79 <jaspreet79@gmail.com>
2018-09-04 17:32:56 +0530

    fixed script

--- src/main/resources/db/db-1/oracle/db_dynamic_property.sql
+++ src/main/resources/db/db-1/oracle/db_dynamic_property.sql
@@ -9,7 +9,7 @@ IF (l_count = 0 ) THEN
     Insert into db_dynamic_property
        (property_id, property_name, default_value, value,modifiable, property_category_id, property_type_id, suite_product_id, description, created_on, updated_on)
     Values
-       (1, 'csa.provider.hostname','localhost','localhost',1, 1,1,1,'authentication provider' CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+       (1, 'csa.provider.hostname','localhost','localhost',1, 1,1,1,'authentication provider',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 END IF;
 
 END;
