32ff1fb57074f2b44373a839e51e7db51656719f
Jaspreet79 <jaspreet79@gmail.com>
2018-09-04 16:34:52 +0530

    seed script for table property_category_name

--- src/main/resources/db/db-1/oracle/db_property_category.sql
+++ src/main/resources/db/db-1/oracle/db_property_category.sql
@@ -1,1 +1,17 @@
 
+DECLARE
+	l_count INTEGER;
+BEGIN
+
+---------------------------Supported Property Category Types---------------
+
+select count(*) into l_count FROM property_category_name where property_category_id = 1 AND property_category_name='OO';
+
+IF (l_count = 0 ) THEN
+    Insert into db_property_type
+       (property_category_id, property_category_name,display_name,description, created_on, updated_on)
+    Values
+       (1,'OO','Operation Orchestration','These properties are used for configuration of OO with CSA', CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+END IF;
+
+END;
