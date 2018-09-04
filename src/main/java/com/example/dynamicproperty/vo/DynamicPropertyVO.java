8c52868bab1d4b30a075bf7a6e99e4a35ac0ef07
Jaspreet79 <jaspreet79@gmail.com>
2018-09-04 21:47:00 +0530

    Create DynamicPropertyVO.java

---
+++ src/main/java/com/example/dynamicproperty/vo/DynamicPropertyVO.java
@@ -0,0 +1,110 @@
+package com.example.dynamicproperty.vo;
+
+import com.example.dynamicproperty.model.PropertyCategory;
+import com.example.dynamicproperty.model.PropertyType;
+import com.example.dynamicproperty.model.SuiteProduct;
+
+import java.util.Date;
+
+public class DynamicPropertyVO {
+    private Long id;
+    private String propertyName;
+    private String description;
+    private String propertyDefaultValue;
+    private String propertyValue;
+    private Boolean modifiable;
+    private PropertyCategory propertyCategory;
+    private PropertyType propertyType;
+    private SuiteProduct suiteProduct;
+    private Date createdOn;
+    private Date updatedOn;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPropertyDefaultValue() {
+        return propertyDefaultValue;
+    }
+
+    public void setPropertyDefaultValue(String propertyDefaultValue) {
+        this.propertyDefaultValue = propertyDefaultValue;
+    }
+
+    public String getPropertyValue() {
+        return propertyValue;
+    }
+
+    public void setPropertyValue(String propertyValue) {
+        this.propertyValue = propertyValue;
+    }
+
+    public Boolean getModifiable() {
+        return modifiable;
+    }
+
+    public void setModifiable(Boolean modifiable) {
+        this.modifiable = modifiable;
+    }
+
+    public PropertyCategory getPropertyCategory() {
+        return propertyCategory;
+    }
+
+    public void setPropertyCategory(PropertyCategory propertyCategory) {
+        this.propertyCategory = propertyCategory;
+    }
+
+    public PropertyType getPropertyType() {
+        return propertyType;
+    }
+
+    public void setPropertyType(PropertyType propertyType) {
+        this.propertyType = propertyType;
+    }
+
+    public SuiteProduct getSuiteProduct() {
+        return suiteProduct;
+    }
+
+    public void setSuiteProduct(SuiteProduct suiteProduct) {
+        this.suiteProduct = suiteProduct;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+}
