5a6ef8af2b4b0097708937f34f638f16005dcf1a
Jaspreet79 <jaspreet79@gmail.com>
2018-09-04 21:38:06 +0530

    Update SuiteProductVO.java

--- src/main/java/com/example/dynamicproperty/vo/SuiteProductVO.java
+++ src/main/java/com/example/dynamicproperty/vo/SuiteProductVO.java
@@ -1,1 +1,61 @@
+package com.example.dynamicproperty.vo;
+
+import java.util.Date;
+
+public class SuiteProductVO {
+    private Long id;
+    private String suiteProductName;
+    private String displayName;
+    private String description;
+    private Date createdOn;
+    private Date updatedOn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSuiteProductName() {
+        return suiteProductName;
+    }
+
+    public void setSuiteProductName(String suiteProductName) {
+        this.suiteProductName = suiteProductName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+}
 
