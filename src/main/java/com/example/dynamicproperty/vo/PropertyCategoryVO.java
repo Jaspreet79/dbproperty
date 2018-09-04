c354a77f0e739ad0cf8c755b8a8ecd814c6cd769
Jaspreet79 <jaspreet79@gmail.com>
2018-09-04 21:42:58 +0530

    Update PropertyCategoryVO.java

--- src/main/java/com/example/dynamicproperty/vo/PropertyCategoryVO.java
+++ src/main/java/com/example/dynamicproperty/vo/PropertyCategoryVO.java
@@ -1,1 +1,61 @@
 
+package com.example.dynamicproperty.vo;
+
+import java.util.Date;
+
+public class PropertyCategoryVO {
+    private Long id;
+    private String propertyCategory;
+    private String displayName;
+    private String description;
+    private Date createdOn;
+    private Date updatedOn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPropertyCategory() {
+        return propertyCategory;
+    }
+
+    public void setPropertyCategory(String propertyCategory) {
+        this.propertyCategory = propertyCategory;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+}
