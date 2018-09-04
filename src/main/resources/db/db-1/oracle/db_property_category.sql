0d03580390a0e54bdddd6885bee5f905d6a301b2
Jaspreet79 <jaspreet79@gmail.com>
2018-09-04 17:28:49 +0530

    fixed table name

--- src/main/resources/db/db-1/oracle/db_property_category.sql
+++ src/main/resources/db/db-1/oracle/db_property_category.sql
@@ -7,7 +7,7 @@ BEGIN
 select count(*) into l_count FROM db_property_category where property_category_id = 1 AND property_category_name='CSA';
 
 IF (l_count = 0 ) THEN
-    Insert into db_property_type
+    Insert into db_property_category
        (property_category_id, property_category_name,display_name,description, created_on, updated_on)
     Values
        (1,'CSA','CLOUD Service Automation','These properties are used for configuration of CSA', CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
