67b65fca080e0855173ece726992df146a9f2c29
Jaspreet79 <jaspreet79@gmail.com>
2018-09-05 15:12:38 +0530

    fixed property_id

--- src/main/resources/db/db-1/oracle/db_dynamic_property.sql
+++ src/main/resources/db/db-1/oracle/db_dynamic_property.sql
@@ -12,7 +12,7 @@ IF (l_count = 0 ) THEN
        (1, 'csa.provider.hostname','localhost','localhost',1, 1,1,1,'authentication provider',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 END IF;
 
-select count(*) into l_count FROM db_dynamic_property where property_id = 1 AND property_name='csa.provider.port';
+select count(*) into l_count FROM db_dynamic_property where property_id = 2 AND property_name='csa.provider.port';
 IF (l_count = 0 ) THEN
     Insert into db_dynamic_property
        (property_id, property_name, default_value, value,modifiable, property_category_id, property_type_id, suite_product_id, description, created_on, updated_on)
