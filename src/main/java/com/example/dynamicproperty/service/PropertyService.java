72181999473468cdb0b3f3770f8ae078c4c71517
Jaspreet79 <jaspreet79@gmail.com>
2018-09-07 18:06:21 +0530

    added getPropertyList method

--- src/main/java/com/example/dynamicproperty/service/PropertyService.java
+++ src/main/java/com/example/dynamicproperty/service/PropertyService.java
@@ -1,1 +1,11 @@
+package com.example.dynamicproperty.service;
 
+
+import com.example.dynamicproperty.model.DynamicProperty;
+
+import java.util.List;
+
+public interface PropertyService {
+
+    public List<DynamicProperty> getPropertyList () throws Exception;
+}
