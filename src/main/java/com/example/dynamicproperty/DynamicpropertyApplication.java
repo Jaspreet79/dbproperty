598b3e96d15c5c31997ce4e2110df9c5810fe491
jaspreet79 <jaspreet79@gmail.com>
2018-09-10 09:53:20 +0530

    swagger support

--- src/main/java/com/example/dynamicproperty/DynamicpropertyApplication.java
+++ src/main/java/com/example/dynamicproperty/DynamicpropertyApplication.java
@@ -8,5 +8,6 @@ public class DynamicpropertyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DynamicpropertyApplication.class, args);
+        System.out.println("Dynmaic DB Properties");
     }
 }
