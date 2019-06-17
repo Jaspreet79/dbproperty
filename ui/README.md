cc9d70e61103205b48854aa9a554c8d0f465eabe
Chhaya Singh <chhayas@microfocus.com>
2019-06-17 11:20:16 +0530

    initial changes

--- ui/README.md
+++ ui/README.md
@@ -1,6 +1,6 @@
 # Ui
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.2.
 
 ## Development server
 
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PropertyComponent } from './components/property/property.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    PropertyComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule,
